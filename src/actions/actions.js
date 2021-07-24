e595419c3e20848f0fcdb6db1710ff2b37e968e6
Numan <nomanmani62@gmail.com>
2021-07-24 08:33:48 -0700

    fetching products with redux thunk completed

---
+++ src/actions/actions.js
@@ -0,0 +1,22 @@
+export const handleLoading = () => {
+  return { type: 'HANDLE_LOADING' };
+};
+
+export const fetchProductsSuccess = (products) => {
+  return { type: 'FETCH_PRODUCTS_SUCCESS', payload: products };
+};
+
+export const fetchProductsFailure = (error) => {
+  return { type: 'FETCH_PRODUCTS_FAILURE', payload: error };
+};
+
+export const fetchProducts = () => async (dispatch) => {
+  dispatch(handleLoading());
+  try {
+    const data = await fetch('http:///fakestoreapi.com/products?limit=10');
+    const products = await data.json();
+    dispatch(fetchProductsSuccess(products));
+  } catch (error) {
+    dispatch(fetchProductsFailure(error));
+  }
+};
