501d36fcf647a72a2900606265f0d962a41d1fec
David Kerr <contactdavidkerr@gmail.com>
2021-07-29 00:14:07 +0100

    react-query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import ProductDetails from './pages/ProductDetails';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/product/:id">
-          <ProductDetails />
-        </Route>
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/product/:id">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -7,37 +7,29 @@ import {
   Flex,
   Heading,
   Image,
-} from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { changeCartCounter, fetchSingleProduct } from '../actions/actions';
-import Loading from '../components/Loading';
+} from "@chakra-ui/react";
+import { useQuery } from "react-query";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const ProductDetails = () => {
   const { id } = useParams();
 
-  const { loading, singleProduct, cartCounter, cartProducts } = useSelector(
-    (state) => state
-  );
-
-  console.log(cartCounter, cartProducts);
-
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchSingleProduct(`http:///fakestoreapi.com/products/${id}`));
-  }, [id, dispatch]);
-
-  if (loading) {
+  const { isLoading, error, data } = useQuery(["products", id], () =>
+    fetch(`http:///fakestoreapi.com/products/${id}`).then((res) => res.json())
+  );
+  if (isLoading) {
     return <Loading />;
   }
 
   const handleCart = () => {
-    dispatch(changeCartCounter(id));
+    // dispatch an action to add the item to the cart
   };
 
-  const { id: productID, title, image, description, price } = singleProduct;
+  const { id: productID, title, image, description, price } = data;
 
   return (
     <Container>
@@ -78,8 +70,8 @@ const ProductDetails = () => {
             <Button
               onClick={() => handleCart()}
               _hover={{
-                bgColor: 'gray.400',
-                transform: 'translateY(-5px)',
+                bgColor: "gray.400",
+                transform: "translateY(-5px)",
               }}
               my="4"
               size="md"
